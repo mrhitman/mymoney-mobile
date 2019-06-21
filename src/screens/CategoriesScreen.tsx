4b7f0791f600461f0975bf6d826ccb4ab2d6bd11
hitman <kabalx47@gmail.com>
2019-06-21 16:42:52 +0300

    customize categories screen

--- src/screens/CategoriesScreen.tsx
+++ src/screens/CategoriesScreen.tsx
@@ -1,12 +1,14 @@
 import { Container, Content, Icon, List, ListItem, Picker, Text, View } from 'native-base';
 import React, { Component } from 'react';
 import { StatusBar, StyleSheet } from 'react-native';
+import uuid from 'uuid';
 
 const categories = [
-  { id: 1, name: 'Food', type: 'outcome' },
-  { id: 2, name: 'Entertainments', type: 'outcome' },
-  { id: 3, name: 'Salary', type: 'income' },
-  { id: 4, name: 'Deposit', type: 'income' }
+  { id: uuid(), name: 'Food', type: 'outcome' },
+  { id: uuid(), name: 'Entertainments', type: 'outcome' },
+  { id: uuid(), name: 'Education', type: 'outcome' },
+  { id: uuid(), name: 'Salary', type: 'income' },
+  { id: uuid(), name: 'Deposit', type: 'income' }
 ];
 
 export class CategoriesScreen extends Component {
@@ -22,9 +24,11 @@ export class CategoriesScreen extends Component {
           <View style={styles.accountHeader}>
             <Text style={styles.accountHeaderText}>YOUR CATEGORIES</Text>
             <Picker
+              mode="dropdown"
+              note
               style={styles.accountHeaderFilter}
               textStyle={styles.accountHeaderFilterText}
-              mode="dropdown"
+              itemTextStyle={styles.accountHeaderFilterText}
               iosIcon={<Icon name="arrow-down" />}
               selectedValue={this.state.selected}
               onValueChange={this.handleChangeFilter}
@@ -37,10 +41,9 @@ export class CategoriesScreen extends Component {
           <View>
             <List>
               {categories
-                .filter(category => {
-                  if (this.state.selected === 'ALL') return true;
-                  return this.state.selected.toLowerCase() === category.type.toLowerCase();
-                })
+                .filter(category =>
+                  this.state.selected === 'ALL' || this.state.selected.toLowerCase() === category.type.toLowerCase()
+                )
                 .map(category => (
                   <ListItem key={category.id}>
                     <Text>{category.name}</Text>
