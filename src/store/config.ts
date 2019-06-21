b55424b2dcd943aa0ee7aec754d7522e64a46fb9
hitman <kabalx47@gmail.com>
2019-06-21 16:28:33 +0300

    remove mobx

--- src/store/config.ts
+++
@@ -1,8 +0,0 @@
-import { types } from 'mobx-state-tree';
-import { Currency } from './currency';
-
-export const Config = types.model({
-  pin: types.string,
-  mainCurrency: types.reference(Currency),
-  server: types.string
-});
--- src/store/profile.ts
+++
@@ -1,9 +0,0 @@
-import { types } from 'mobx-state-tree';
-
-export const Profile = types.model({
-  name: types.string,
-  last_name: types.string,
-  email: types.string,
-  birthday: types.Date,
-  updated_at: types.number
-});
