0435dcd1f9507335e56a325a9546faa16889c48b
hitman <kabalx47@gmail.com>
2019-06-24 00:38:17 +0300

    form

--- src/screens/TransactionAddScreen.tsx
+++ src/screens/TransactionAddScreen.tsx
@@ -1,12 +1,25 @@
-import { Text, View } from 'native-base';
+import { Container, Content, Form, Input, Item, Label, Text } from 'native-base';
 import React, { Component } from 'react';
 
 export class TransactionAddScreen extends Component {
   render() {
     return (
-      <View>
-        <Text> textInComponent </Text>
-      </View>
+      <Container>
+        <Content>
+          <Form>
+            <Item inlineLabel>
+              <Label>
+                <Text>Some text</Text>
+              </Label>
+              <Input
+                keyboardType="decimal-pad"
+                value="0"
+                style={{ fontSize: 40, margin: 10, fontWeight: 'bold' }}
+              />
+            </Item>
+          </Form>
+        </Content>
+      </Container>
     );
   }
 }
