920236ea3365be86d61e9d306929d5198915355b
hitman <kabalx47@gmail.com>
2019-06-24 12:52:08 +0300

    add trx pickers

--- src/screens/TransactionAddScreen.tsx
+++ src/screens/TransactionAddScreen.tsx
@@ -1,27 +1,146 @@
-import { Container, Content, Form, Input, Item, Label, Text } from 'native-base';
+import { Container, Content, Form, Input, Item, Label, Text, Icon, View } from 'native-base';
 import React, { Component } from 'react';
+import { Picker } from 'react-native';
+import { connect } from 'react-redux';
+import ICategory from '../types/Category';
+import IWallet from '../types/Wallet';
 
-export class TransactionAddScreen extends Component {
-  render() {
+interface TransactionAddScreenProps {
+  wallets: IWallet[];
+  categories: ICategory[];
+}
+
+enum Operations {
+  income,
+  outcome,
+  transfer
+}
+
+const OperationsSymbols = {
+  [Operations.income]: 'plus',
+  [Operations.outcome]: 'minus',
+  [Operations.transfer]: 'swap',
+};
+
+interface TransactionAddScreenState {
+  operation: Operations;
+}
+
+export class TransactionAddScreen extends Component<
+  TransactionAddScreenProps,
+  TransactionAddScreenState
+> {
+  public state = {
+    operation: Operations.outcome
+  };
+
+  public render() {
+    const { categories, wallets } = this.props;
     return (
       <Container>
-        <Content>
+        <Content padder style={{ margin: 10 }}>
           <Form>
-            <Item inlineLabel>
-              <Label>
-                <Text>Some text</Text>
-              </Label>
+            <Item inlineLabel >
+              <View onTouchEndCapture={this.handleChangeOperation} >
+                <Label style={{ marginRight: 25 }} >
+                    <Icon
+                      type="Entypo"
+                      name={OperationsSymbols[this.state.operation]}
+                      style={{ fontSize: 50, lineHeight: 80, height: 120, color: 'rgba(255,0,0,0.65)' }}
+                    />
+                </Label>
+              </View>
               <Input
                 keyboardType="decimal-pad"
-                value="0"
-                style={{ fontSize: 40, margin: 10, fontWeight: 'bold' }}
+                placeholder="0"
+                style={{
+                  fontSize: 65,
+                  fontWeight: 'bold',
+                  lineHeight: 80,
+                  height: 130
+                }}
               />
+            </Item>
+            <Item picker fixedLabel>
+              <Label>
+                <Text>From wallet:</Text>
+              </Label>
+              <Picker
+                mode="dropdown"
+                onValueChange={() => ({})}
+                style={{ flex: 1 }}
+              >
+                {wallets.map(wallet => (
+                  <Picker.Item
+                    key={wallet.id}
+                    label={wallet.name}
+                    value={wallet.id}
+                  />
+                ))}
+              </Picker>
+            </Item>
+            {this.state.operation === Operations.transfer && <Item picker fixedLabel>
+                <Label>
+                  <Text>To wallet:</Text>
+                </Label>
+                <Picker
+                  mode="dropdown"
+                  onValueChange={() => ({})}
+                  style={{ flex: 1 }}
+                >
+                  {wallets.map(wallet => (  // @TODO Filter FROM wallet
+                    <Picker.Item
+                      key={wallet.id}
+                      label={wallet.name}
+                      value={wallet.id}
+                    />
+                  ))}
+                </Picker>
+              </Item>
+            }
+            <Item picker fixedLabel>
+              <Label>
+                <Text>Category:</Text>
+              </Label>
+              <Picker
+                mode="dropdown"
+                onValueChange={() => ({})}
+                style={{ flex: 1 }}
+              >
+                {categories.map(category => (
+                  <Picker.Item
+                    key={category.id}
+                    label={category.name}
+                    value={category.id}
+                  />
+                ))}
+              </Picker>
+            </Item>
+            <Item fixedLabel>
+              <Label>Description:</Label>
+              <Input />
             </Item>
           </Form>
         </Content>
       </Container>
     );
   }
+
+  protected handleChangeOperation = () => {
+    this.setState(state => {
+      switch (state.operation) {
+        case Operations.income:
+          return { ...state, operation: Operations.outcome };
+        case Operations.outcome:
+          return { ...state, operation: Operations.transfer };
+        case Operations.transfer:
+          return { ...state, operation: Operations.income };
+      }
+    });
+  };
 }
 
-export default TransactionAddScreen;
+export default connect(
+  state => state,
+  () => ({})
+)(TransactionAddScreen);
