0fe39cfe2156c5f56e41de1f8fb0efeaa035e82f
mrhitman <kabalx47@gmail.com>
2019-06-30 13:57:11 +0300

    selector trx

--- src/store/selectors/transactions.ts
+++ src/store/selectors/transactions.ts
@@ -1,13 +1,19 @@
 import { DateTime } from 'luxon';
 import Store from '../../types/Store';
+import { groupBy } from 'lodash';
+import ITransaction from '../../types/Transaction';
 
 export const getAmountForPeriod = (state: Store, to: DateTime, from?: DateTime) => {
 	return getForPeriod(state, to, from).reduce((acc, trx) => trx.amount + acc, 0);
 };
 
 export const getForPeriod = (state: Store, to: DateTime, from?: DateTime) => {
-	return state.transactions.filter((trx) => {
+	return state.transactions.filter((trx: ITransaction) => {
 		const date = DateTime.fromJSDate(trx.date);
 		return date.diff(to).seconds > 0 && (from ? date.diff(from) : date.diffNow()).seconds < 0;
 	});
+};
+
+export const groupByCategory = (state: Store) => {
+	return groupBy(state.transactions, 'category_id');
 };
